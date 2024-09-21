ea7aa1d6608ed4517ec4c380b257e36b5f5ef7f0
sergey-dev-s <sunelistan@gmail.com>
2024-09-21 20:20:39 +0300

    fix

--- pkg/authserv/handlers/handlers.go
+++ pkg/authserv/handlers/handlers.go
@@ -82,6 +82,12 @@ func Login(repo repository.AuthRepository) http.HandlerFunc {
 			return
 		}
 
+		if creds.Username == "" || creds.Password == "" {
+			fncLogger.Error("Bad request:", err)
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
 		storedPasswordHash, err := repo.GetUser(creds.Username)
 		if err != nil || bcrypt.CompareHashAndPassword([]byte(storedPasswordHash), []byte(creds.Password)) != nil {
 			fncLogger.Error("Unauthorized:", err)
