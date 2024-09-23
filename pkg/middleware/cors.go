7f1bc368d1fbff3afb58b6c4b6e366b9621a928f
sergey-dev-s <sunelistan@gmail.com>
2024-09-23 20:47:16 +0300

    fix

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Устанавливаем заголовки CORS
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
